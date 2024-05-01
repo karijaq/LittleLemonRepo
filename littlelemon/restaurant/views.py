32e914c828ed2886722ffcaa9d154a907cb362a0
Karina Jacuinde <kjacuinde09@gmail.com>
2024-05-01 14:25:03 -0500

    auth changes

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -7,6 +7,10 @@ from rest_framework import viewsets
 
 from .models import Menu, Booking
 from .serializers import menuSerializer, bookingSerializer
+
+from rest_framework.permissions import IsAuthenticated
+#from rest_framework.decorators import api_view, permission_classes
+
 # Create your views here.
 
 def index(request):
@@ -20,6 +24,12 @@ class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = menuSerializer
 
+#@api_view
+#@permission_classes([IsAuthenticated])    
+    
+
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = bookingSerializer
+    
